d35174579786caf3124ff5967491942524d62446
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-25 13:58:31 +0300

    Вариант импорта через * as

--- package/index.js
+++ package/index.js
@@ -1,6 +1,7 @@
-import { sum, addOne as add1 } from "./first";
+// import { sum, addOne as add1 } from "./first";
+import * as first from "./first";
 import { firstName } from "./second";
 
 console.log("Hello from entry point", firstName);
-console.log(sum(2, 4));
-console.log(add1(5));
+console.log(first.sum(2, 4));
+console.log(first.addOne(5));
