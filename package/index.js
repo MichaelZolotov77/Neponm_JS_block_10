41aaea080555e9caeb7fde8a2b101b988a492301
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-25 13:55:21 +0300

    Вариант импорта с переименованием as

--- package/index.js
+++ package/index.js
@@ -1,4 +1,6 @@
-import { sum } from "./first";
+import { sum, addOne as add1 } from "./first";
+import { firstName } from "./second";
 
-console.log("Hello from entry point");
+console.log("Hello from entry point", firstName);
 console.log(sum(2, 4));
+console.log(add1(5));
